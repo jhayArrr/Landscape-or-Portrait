9d5469fe0525b857b19b660d2350de84019afd72
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-11 13:12:16 -0600

    changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function isLandscape (4, 3) {
+function isLandscape (___, ___) {
   if (window.innerheight == window.innerwidth) return 'Square'; //must be first, return will not allow rest of code to run
   return (window.innerwidth > window.innerheight) ? 'Landscape' : 'Portrait'; //returns the value of the expression
 }
+
+let window.innerwidth= screen.width;
+let window.innerheight = screen.height;
 
 console.log("This display geometry is", isLandscape (window.innerwidth, window.innerheight)); //Difference between calling functions with arguements and sending to parameter's, local variables

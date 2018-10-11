7de90041522cf2e72d2d866421c93db13aaa30ed
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-11 13:05:57 -0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function isLandscape (window.innerheight, window.innerwidth) {
+function isLandscape (,) {
   if (window.innerheight == window.innerwidth) return 'Square'; //must be first, return will not allow rest of code to run
   return (window.innerwidth > window.innerheight) ? 'Landscape' : 'Portrait'; //returns the value of the expression
 }

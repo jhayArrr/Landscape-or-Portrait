442070f3e62d0292514d3910e1523a85594885b8
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-11 11:24:51 -0600

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+function isLandscape ( height > width) {
+  if (height == width) return 'Square'; //must be first, return will not allow rest of code to run
+  return (width > height) ? 'Landscape' : 'Portrait'; //returns the value of the expression
+}
+
+let width = 1200;
+let height = 800;
+console.log("This display geometry is", isLandscape (width, height)); //Difference between calling functions with arguements and sending to parameter's, local variables

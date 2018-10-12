7e097e355c8268631bd061b5effa32aa6b50baab
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-12 12:54:24 -0600

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function isLandscape (___, ___) {
-  if (window.innerheight == window.innerwidth) return 'Square'; //must be first, return will not allow rest of code to run
-  return (window.innerwidth > window.innerheight) ? 'Landscape' : 'Portrait'; //returns the value of the expression
+var orientation = screen.msOrientation || (screen.orientation || screen.mozOrientation || {}).type;
+
+if (orientation === "landscape-primary") {
+  console.log("That looks good.");
+} else if (orientation === "landscape-secondary") {
+  console.log("Mmmh... the screen is upside down!");
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  console.log("Mmmh... you should rotate your device to landscape");
+} else if (orientation === undefined) {
+ console.log("The orientation API isn't supported in this browser :(");
 }
-
-let width= 4;
-let height = 3;
-
-console.log("This display geometry is", isLandscape (window.innerwidth, window.innerheight)); //Difference between calling functions with arguements and sending to parameter's, local variables
